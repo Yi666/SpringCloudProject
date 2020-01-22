eae3f08691865c5565087cd9a64b6d444610be11
Yi Liu <yliu1010@ymd.edu>
2020-01-21 21:49:13 -0500

    first commit

---
+++ limits-service/src/main/java/com/microservices/limitsservice/LimitsConfiguration.java
@@ -0,0 +1,31 @@
+package com.microservices.limitsservice;
+
+
+public class LimitsConfiguration {
+    private int minimum;
+    private int maximum;
+
+    public LimitsConfiguration(int minimum, int maximum) {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+
+    public LimitsConfiguration() {
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+}
